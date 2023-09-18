dbc719991a2d8ba19c646c9c0cfd101619ff0f7e
Paul Gung <gongshaoxu@gmail.com>
2023-09-18 21:26:15 +0800

    feat: add cros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { ThsModule } from './ths/ths.module';
+import { cors } from 'cors';
 
 @Module({
   imports: [ThsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // 允许所有域名跨域访问
+    consumer.apply(cors()).forRoutes('*');
+  }
+}
